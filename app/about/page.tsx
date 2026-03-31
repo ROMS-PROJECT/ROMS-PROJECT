d1a309a6f9c68349c144af3ad9f1ee9b5b0a7fd8
minyounki <minyounki@gmail.com>
2026-04-01 00:09:05 +0900

    맵별데이터 html 만듬

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,48 @@
+"use client";
+
 import Test from './test';
+import { useEffect, useState } from "react";
+
+type User = {
+    id: number;
+    name: string;
+};
 
 export default function About() {
+    const [data, setData] = useState<User[]>([]);
+
+    useEffect(() => {
+        fetch("/api/supabase")
+            .then((res) => res.json())
+            .then((result) => {
+                setData(result);
+            });
+    }, []);
+
     return (
-        <div>
+        <>
             <h1>여기는 About 페이지 😆</h1>
             <Test />
-        </div>
+
+
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+
+
     );
 }
