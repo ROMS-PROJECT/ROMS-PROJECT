eae1efc182f652fc20db4b340f5f6ce2f9b9d6af
minyounki <minyounki@gmail.com>
2026-04-08 00:32:31 +0900

    유저 db가져와 출력

--- app/userSearchMap/page.tsx
+++ app/userSearchMap/page.tsx
@@ -1,14 +1,28 @@
-export default function userSearchMap() {
-  const data = [
-    { map: "리장타워", total: 50, win: 30, lose: 20, rate: "60%" },
-    { map: "부산", total: 20, win: 10, lose: 10, rate: "50%" },
-    { map: "수라바사", total: 40, win: 30, lose: 10, rate: "75%" },
-  ];
+"use client";
+import { useEffect, useState } from 'react';
+import { supabase } from '@/lib/supabase';
+
+export default function UserSearchMap() {
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const { data } = await supabase
+                .from('tb_player')
+                .select('player_name')
+                .eq('player_name', '이선생') // 👈 where 조건
+                .single(); // 👈 결과가 1개만 있어야 정상
+
+            if (data) setUsername(data.player_name);
+        };
+
+        fetchUser();
+    }, []);
 
 
     return (
         <>
-            <div className="min-h-screen bg-gray-100 flex justify-center">
+            <div className="min-h-screen flex justify-center">
                 <div className="w-full max-w-6xl p-10">
 
 
@@ -26,6 +40,7 @@ export default function userSearchMap() {
 
                             {/* 이름 + 아이콘 */}
                             <div>
+                                <p className="font-bold">{username}</p>
                                 <p className="font-bold">소우릎</p>
                                 <div className="flex gap-2 mt-1">
                                     <img src="/z.png" className="w-6 h-6" />
@@ -56,107 +71,101 @@ export default function userSearchMap() {
                     </div>
 
 
-        <div className="flex items-center gap-6 p-4 bg-gray-200">
-          {/* 왼쪽 프로필*/}
-          {/* src위치는 default로 public에서 찾는다*/}
-          <img src="/profile.png" alt="profile" className="w-20 h-20 rounded" />
 
-          {/* 오른쪽 전체 영역 */}
-          <div className="flex flex-col gap-3">
-            {/* 이름 + 아이콘 */}
-            <div>
-              <p className="font-bold">소우릎</p>
-              <div className="flex gap-2 mt-1">
-                <img src="/z.png" className="w-6 h-6" />
-                <img src="/youtube.png" className="w-6 h-6" />
-              </div>
-            </div>
 
-                    <div className="grid grid-cols-2 gap-6">
 
-                        {/* 왼쪽 영역 */}
-                        <div className="flex flex-col gap-6">
 
-                            {/* 가장 많이 사용한 영웅 */}
-                            <div className="border p-4 bg-white">
-                                <p className="font-bold mb-2 text-center">가장 많이 사용한 영웅 Top3</p>
-                                <div className="flex justify-between">
-                                    <div className="text-center">
-                                        <p>Top1</p>
-                                        <img src="/hero1.png" className="w-16 h-16 mx-auto" />
-                                        <p>60번</p>
-                                    </div>
-                                    <div className="text-center">
-                                        <p>Top2</p>
-                                        <img src="/hero2.png" className="w-16 h-16 mx-auto" />
-                                        <p>30번</p>
-                                    </div>
-                                    <div className="text-center">
-                                        <p>Top3</p>
-                                        <img src="/hero3.png" className="w-16 h-16 mx-auto" />
-                                        <p>25번</p>
+                    {/* 오른쪽 전체 영역 */}
+                    <div className="flex flex-col gap-3">
+
+
+                        <div className="grid grid-cols-2 gap-6">
+
+                            {/* 왼쪽 영역 */}
+                            <div className="flex flex-col gap-6">
+
+                                {/* 가장 많이 사용한 영웅 */}
+                                <div className="border p-4 bg-white">
+                                    <p className="font-bold mb-2 text-center">가장 많이 사용한 영웅 Top3</p>
+                                    <div className="flex justify-between">
+                                        <div className="text-center">
+                                            <p>Top1</p>
+                                            <img src="/hero1.png" className="w-16 h-16 mx-auto" />
+                                            <p>60번</p>
+                                        </div>
+                                        <div className="text-center">
+                                            <p>Top2</p>
+                                            <img src="/hero2.png" className="w-16 h-16 mx-auto" />
+                                            <p>30번</p>
+                                        </div>
+                                        <div className="text-center">
+                                            <p>Top3</p>
+                                            <img src="/hero3.png" className="w-16 h-16 mx-auto" />
+                                            <p>25번</p>
+                                        </div>
                                     </div>
                                 </div>
+
+                                {/* 승률 높은 맵 */}
+                                <div className="border p-4 bg-white">
+                                    <p className="font-bold mb-2 text-center">승률 높은 맵순위 Top3</p>
+                                    <div className="flex justify-between">
+                                        <div className="text-center">
+                                            <p>Top1</p>
+                                            <img src="/map1.png" className="w-20 h-12 mx-auto" />
+                                            <p>90%</p>
+                                        </div>
+                                        <div className="text-center">
+                                            <p>Top2</p>
+                                            <img src="/map2.png" className="w-20 h-12 mx-auto" />
+                                            <p>80%</p>
+                                        </div>
+                                        <div className="text-center">
+                                            <p>Top3</p>
+                                            <img src="/map3.png" className="w-20 h-12 mx-auto" />
+                                            <p>65%</p>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
 
-                            {/* 승률 높은 맵 */}
-                            <div className="border p-4 bg-white">
-                                <p className="font-bold mb-2 text-center">승률 높은 맵순위 Top3</p>
-                                <div className="flex justify-between">
-                                    <div className="text-center">
-                                        <p>Top1</p>
-                                        <img src="/map1.png" className="w-20 h-12 mx-auto" />
-                                        <p>90%</p>
-                                    </div>
-                                    <div className="text-center">
-                                        <p>Top2</p>
-                                        <img src="/map2.png" className="w-20 h-12 mx-auto" />
-                                        <p>80%</p>
-                                    </div>
-                                    <div className="text-center">
-                                        <p>Top3</p>
-                                        <img src="/map3.png" className="w-20 h-12 mx-auto" />
-                                        <p>65%</p>
+                            {/* 오른쪽 영역 */}
+                            <div className="flex flex-col gap-6">
+
+                                {/* 모든 경기 누적 */}
+                                <div className="border p-4 bg-white">
+                                    <p className="font-bold mb-2 text-center">모든 경기 누적합</p>
+                                    <div className="grid grid-cols-6 gap-2 text-center">
+                                        <div>총킬<br />60</div>
+                                        <div>총도움<br />60</div>
+                                        <div>총죽음<br />60</div>
+                                        <div>총피해량<br />60</div>
+                                        <div>총치유<br />60</div>
+                                        <div>총경감<br />60</div>
                                     </div>
                                 </div>
-                            </div>
 
-                        </div>
-
-                        {/* 오른쪽 영역 */}
-                        <div className="flex flex-col gap-6">
-
-                            {/* 모든 경기 누적 */}
-                            <div className="border p-4 bg-white">
-                                <p className="font-bold mb-2 text-center">모든 경기 누적합</p>
-                                <div className="grid grid-cols-6 gap-2 text-center">
-                                    <div>총킬<br />60</div>
-                                    <div>총도움<br />60</div>
-                                    <div>총죽음<br />60</div>
-                                    <div>총피해량<br />60</div>
-                                    <div>총치유<br />60</div>
-                                    <div>총경감<br />60</div>
+                                {/* 경기당 최대 */}
+                                <div className="border p-4 bg-white">
+                                    <p className="font-bold mb-2 text-center">경기당 최대</p>
+                                    <div className="grid grid-cols-6 gap-2 text-center">
+                                        <div>킬<br />60</div>
+                                        <div>도움<br />60</div>
+                                        <div>죽음<br />60</div>
+                                        <div>피해량<br />60</div>
+                                        <div>치유<br />60</div>
+                                        <div>경감<br />60</div>
+                                    </div>
                                 </div>
-                            </div>
 
-                            {/* 경기당 최대 */}
-                            <div className="border p-4 bg-white">
-                                <p className="font-bold mb-2 text-center">경기당 최대</p>
-                                <div className="grid grid-cols-6 gap-2 text-center">
-                                    <div>킬<br />60</div>
-                                    <div>도움<br />60</div>
-                                    <div>죽음<br />60</div>
-                                    <div>피해량<br />60</div>
-                                    <div>치유<br />60</div>
-                                    <div>경감<br />60</div>
-                                </div>
                             </div>
 
                         </div>
 
                     </div>
-
                 </div>
+
             </div>
         </>
     );
