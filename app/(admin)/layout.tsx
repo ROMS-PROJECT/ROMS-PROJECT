6ff3de669dbf924941322bc5374261939172424e
junhyeon0325 <jjh990325@gmail.com>
2026-06-16 00:09:48 +0900

    JJH_20260616_HOME refactor: 사용자관리, 통계보기 메뉴 SVG수정

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -49,10 +49,12 @@ const ADMIN_MENUS = [
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <path d="M3 3v18h18" />
-        <path d="M18 17V9" />
-        <path d="M13 17V5" />
-        <path d="M8 17v-3" />
+        <path d="M8 2v4" />
+        <path d="M16 2v4" />
+        <path d="M21 13V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8" />
+        <path d="M3 10h18" />
+        <path d="M16 19h6" />
+        <path d="M19 16v6" />
       </svg>
     ),
   },
@@ -71,12 +73,10 @@ const ADMIN_MENUS = [
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <path d="M8 2v4" />
-        <path d="M16 2v4" />
-        <path d="M21 13V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8" />
-        <path d="M3 10h18" />
-        <path d="M16 19h6" />
-        <path d="M19 16v6" />
+        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+        <circle cx="9" cy="7" r="4" />
+        <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
       </svg>
     ),
   },
